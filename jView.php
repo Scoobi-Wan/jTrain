9cfe348c490caefc9dc235b086fc989520494de0
Paul Rich <37647116+Scoobi-Wan@users.noreply.github.com>
2020-12-19 21:27:57 -0700

    Create jView.php
    Created jView.php which will serve as a landing page.

---
+++ jView.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>jTrain</title>
+<link rel="stylesheet" type="text/css" href="jStyles.css">
+</head>
+<body onload="showQuotes()">
+<h1>Welcome to jTrain!</h1>
+<h2>A Jeopardy themed trivia training website.</h2>
+<br>
+
+<?php
+echo "<input type='submit' name='Login' class='loginButton' value='Login'/>";
+
+?>
+</body>
+</html>
