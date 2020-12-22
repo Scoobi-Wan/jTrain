0c7b0913e6debf417ef0f47dcc94237d9b1e4bdd
Paul Rich <37647116+Scoobi-Wan@users.noreply.github.com>
2020-12-22 16:59:22 -0700

    Created jRegister.php
    Allows users to register a profile

---
+++ jRegister.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register</title>
+<link rel="stylesheet" type="text/css" href="jStyles.css">
+<!-- Author: Paul Rich -->
+</head>
+<body>
+
+<?php session_start() ?>
+
+<h1>Welcome to jTrain!</h1>
+<h2>A Jeopardy themed trivia training website.</h2>
+<form autocomplete="off"  action="jController.php" method="post">
+<div class="registerContainer">
+<h3>Registration</h3>
+<input type="text" name="registerUsername" placeholder='Username' required>
+<br>
+<input type="password" name="registerPassword" placeholder='Password' required>
+<br><br>
+<input type="submit" value="Register"> <br>
+<div id="nameTakenDiv"></div>
+
+<?php 
+if( isset($_SESSION ['registrationError']))
+  echo $_SESSION ['registrationError']; 
+unset($_SESSION ['registrationError']);
+
+
+?>
+
+</div>
+
+</form>
+</body>
+</html>
